92629aae6e77294530b648d39b2c21a447299dae
Christian Bick <christian.bick@bitsuppliers.com>
2025-10-03 18:17:11 +0200

    Adding recommendations

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,88 +1,86 @@
-# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
-# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
 
-resource "google_compute_instance" "instance-20251003-151201" {
+provider "google" {
+  project = var.project_id
+  zone    = var.zone
+}
+
+# Use a local variable to derive the region from the zone (e.g., "us-central1-a" -> "us-central1")
+locals {
+  region = join("-", slice(split("-", var.zone), 0, 2))
+}
+
+# Data source to automatically find the latest PyTorch GPU image
+data "google_compute_image" "pytorch_image" {
+  project = "deeplearning-platform-release"
+  family  = "pytorch-latest-gpu"
+}
+
+# Define the Compute Engine VM instance
+resource "google_compute_instance" "qwen_training_vm" {
+  name         = var.instance_name
+  machine_type = "g2-standard-8"
+  zone         = var.zone
+
+  # Boot disk configured to use the latest Deep Learning image
   boot_disk {
-    auto_delete = true
-    device_name = var.instance_name
-
     initialize_params {
-      image = "projects/ml-images/global/images/c0-deeplearning-common-cu124-v20250325-debian-11-py310-conda"
-      size  = 100
+      image = data.google_compute_image.pytorch_image.self_link
+      size  = 100 # GB
       type  = "pd-balanced"
     }
-
-    mode = "READ_WRITE"
+    auto_delete = true
   }
 
+  # Attach the L4 GPU
+  guest_accelerator {
+    type  = "nvidia-l4" # Corrected short name for the type
+    count = 1
+  }
+
+  # Configure as a Spot VM for cost savings
+  scheduling {
+    provisioning_model   = "SPOT"
+    on_host_maintenance = "TERMINATE"
+    automatic_restart   = false # Must be false for Spot VMs
+  }
+
+  # Metadata to install the GPU driver on first boot
+  metadata = {
+    install-gpu-driver = "True"
+    enable-osconfig    = "TRUE"
+  }
+
+  # Network interface with parameterized subnetwork
+  network_interface {
+    network    = "default"
+    subnetwork = "projects/${var.project_id}/regions/${local.region}/subnetworks/default"
+    access_config {}
+  }
+
+  # Service account with broad permissions to avoid issues
+  service_account {
+    email  = "default"
+    scopes = ["cloud-platform"]
+  }
+
+  # Other settings from your configuration
   can_ip_forward      = false
   deletion_protection = false
   enable_display      = false
-
-  guest_accelerator {
-    count = 1
-    type  = "projects/${var.project_id}/zones/${var.zone}/acceleratorTypes/nvidia-l4"
-  }
-
   labels = {
-    goog-ec-src           = "vm_add-tf"
-    goog-ops-agent-policy = "v2-x86-template-1-4-0"
+    goog-ec-src = "vm_add-tf"
   }
-
-  machine_type = "g2-standard-8"
-
-  metadata = {
-    enable-osconfig = "TRUE"
-  }
-
-  name = var.instance_name
-
-  network_interface {
-    access_config {
-      network_tier = "PREMIUM"
-    }
-
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
-    subnetwork  = "projects/${var.project_id}/regions/${var.zone}/subnetworks/default"
-  }
-
-  scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "TERMINATE"
-    preemptible         = false
-    provisioning_model  = "STANDARD"
-  }
-
-  service_account {
-    email  = "575953891979-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
-  }
-
   shielded_instance_config {
     enable_integrity_monitoring = false
     enable_secure_boot          = false
     enable_vtpm                 = false
-  }
-
-  zone = var.zone
-}
-
-module "ops_agent_policy" {
-  source          = "github.com/terraform-google-modules/terraform-google-cloud-operations/modules/ops-agent-policy"
-  project         = var.project_id
-  zone            = var.zone
-  assignment_id   = "goog-ops-agent-v2-x86-template-1-4-0-${var.zone}"
-  agents_rule = {
-    package_state = "installed"
-    version = "latest"
-  }
-  instance_filter = {
-    all = false
-    inclusion_labels = [{
-      labels = {
-        goog-ops-agent-policy = "v2-x86-template-1-4-0"
-      }
-    }]
   }
 }
